bbbd74171e04c822c80c8756196b1d20c3e615bc
jpc0023 <jpc0023@uah.edu>
2022-04-11 19:55:38 -0500

    Updates

---
+++ precache-manifest.c775fe0c7567955984dd366f314fbd7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dd184b3af74d0d16858",
+    "url": "/my-app/static/js/main.0dd184b3.chunk.js"
+  },
+  {
+    "revision": "0b6c93ccb7f017888520",
+    "url": "/my-app/static/css/1.85a4f529.chunk.css"
+  },
+  {
+    "revision": "0b6c93ccb7f017888520",
+    "url": "/my-app/static/js/1.0b6c93cc.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "d7be261131ef0fcdf71c692454702de8",
+    "url": "/my-app/index.html"
+  }
+];
